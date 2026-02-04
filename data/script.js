e483c2fedf6ba4dad0df231543ca7a6074abd03c
pierrepressure <pierreawad10@gmail.com>
2026-02-04 18:13:23 -0500

    add functional static website

---
+++ data/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const button = document.getElementsByClassName('button1');
+
+	button.addEventListener('click', function () {
+		//something here idec
+	})
+	
+
+})
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,55 @@
 #include <Arduino.h>
+#include <Wifi.h>
+#include <SPIFFS.h>
+#include <ESPAsyncWebServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+const char* ssid = "LIVING";
+const char* password = "CAEE22FF";
+const int port = 80;
+
+AsyncWebServer server(port);  // Changed from NetworkServer
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  Serial.printf("Connecting to: %s", ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Initialize SPIFFS
+  SPIFFS.begin(true);
+
+    // Serve static files from SPIFFS
+  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+
+  // Handle 404
+  server.onNotFound([](AsyncWebServerRequest* request) {
+    request->send(404, "text/plain", "Not found");
+    });
+
+    // Start server
+  server.begin();
+  Serial.println("\nHTTP Server Started!");
+  Serial.print("WIFI CONNECTED: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  //nothing needed here for now
+
+  // WiFiClient client = server.available();
+
+
+  // if (client) {
+  //   Serial.println("New Client!")
+  // }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
